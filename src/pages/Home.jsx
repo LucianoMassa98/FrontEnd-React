4ed0252ae44ff34e913872879d37287f9fc7b859
lucianomassa <setigex.sj@hotmail.com>
2022-03-12 19:02:22 -0300

    atomic desing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
+import ProductList from '../containers/ProductList';
 
-const Home = () =>{
-    return(
-        <div>
-            <Header/>
-
-        </div>
-    );
+const Home = () => {
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
+
 export default Home;
+
