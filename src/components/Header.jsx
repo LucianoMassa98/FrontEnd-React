6020518eca75a7bda49e5da640b39db8dae35222
lucianomassa <setigex.sj@hotmail.com>
2022-03-30 19:18:35 -0300

    useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../styles/Header.scss';
-
+import Menu from '@components/Menu.jsx';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shopingcart from '@icons/icon_shopping_cart.svg';
 const Header = () => {
+
+	const [toggle,setToggle] = useState(false);
+
+	const handleToggle=()=>{
+		setToggle(!toggle);
+	}
 	return (
 		<nav>
-			<img src={menu} alt="menu" className="menu" />
+			<img src={menu} alt="menu" className="menu"/>
 			<div className="navbar-left">
 				<img src={logo} alt="logo" className="nav-logo" />
 				<ul>
@@ -33,13 +39,16 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">platzi@example.com</li>
+					<li className="navbar-email" onClick={handleToggle}>
+						platzi@example.com
+					</li>
 					<li className="navbar-shopping-cart">
 						<img src={shopingcart} alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
 			</div>
+			{toggle ? <Menu />: null}
 		</nav>
 	);
 }
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+const API = 'https://api.escuelajs.co/api/v1/products'; 
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+	/*funcion anonima
+	useEffect((async)=>{
+	//apoyar en un recurso que se llama axios nos ayuda a realizar peticiones (instalar)
+		const response = await axios.get(API);
+		setProducts(response.data);
+		},[]);*/
+	//arreglo donde podemos tener diferentes elementos o valores, los cuales estaraescuchando para uncambio
+	
+	useEffect(() => {
+		getData();
+		}, []);
+		
+		const getData = async () => {
+		const response = await axios.get(API);
+		setProducts(response.data);
+		console.log(response);
+	}
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+
+				))}
+				
 			</div>
 		</section>
 	);
